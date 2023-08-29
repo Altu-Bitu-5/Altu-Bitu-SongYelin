ed6760c60890c8eb1013cc159a3fce58f75c952b
yelin56 <100378349+yelin56@users.noreply.github.com>
2023-08-30 06:50:38 +0900

    [스택_큐_덱] 8월 30일

--- 02_스택_큐_덱/1158.cpp
+++ 02_스택_큐_덱/1158.cpp
@@ -14,7 +14,7 @@ int main()
 
     cin >> n >> k;
     queue<int> circle;     // 사람들이 앉을 원
-    queue<int> death_note; // 죽일 명당
+    queue<int> death_note; // 데스노트
     for (int i = 1; i <= n; i++)
     {
         circle.push(i); // 앉히기
@@ -24,15 +24,17 @@ int main()
     int tmp;
     while (n--)
     {
-        for (int i = 1; i < k; i++)
+        for (int i = 1; i < k; i++) // k-1명의 사람을 큐에서 빼서 다시 큐에 넣는다
         {
             tmp = circle.front();
             circle.pop();
             circle.push(tmp);
         }
-        death_note.push(circle.front());
+        death_note.push(circle.front()); // k번째 사람은 데스노트 큐에 저장
         circle.pop();
     }
+
+    // 데스노트에 있는 사람들을 출력
     cout << "<";
     cout << death_note.front();
     death_note.pop();
