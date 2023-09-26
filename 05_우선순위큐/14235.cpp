8315d46926c7f968664f577c0b4869f2c4411102
yelin56 <100378349+yelin56@users.noreply.github.com>
2023-09-27 03:00:21 +0900

    {우선순위큐] 9월 27일

--- 05_우선순위큐/14235.cpp
+++ 05_우선순위큐/14235.cpp
@@ -1,40 +1,33 @@
-z#include <iostream>
+#include <iostream>
+#include <vector>
 #include "queue"
 
 using namespace std;
 
-void calculate(priority_queue<int> &heap,int a){
-
-    int b;
-    if(a==0){
-        if(heap.empty()){//아무것도 없으면
-            cout<<"-1\n";
-        }
-        else{//가장 좋은 선물
-            cout<<heap.top()<<'\n';
-            heap.pop();
-        }
-    }
-    else{//a가 0이 아니다
-        while(a--){
-            cin>>b;//충전
-            heap.push(b);
-        }
-    }
-}
-
 int main() {
 
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n,a;
+    int n;
     priority_queue<int> heap;
-    cin>>n;//입력
+    cin>>n;
     while(n--){
-        cin>>a;//입력
-        calculate(heap,a);//연산
+        int a;
+        cin>>a;
+        if(a==0){
+            if(heap.empty()){
+                cout<<"-1\n";
+            }
+            else{
+                cout<<heap.top()<<'\n';
+                heap.pop();
+            }
+        }
+        else{
+            while(a--){
+                int b;
+                cin>>b;//충전
+                heap.push(b);
+            }
+        }
     }
 
     return 0;
