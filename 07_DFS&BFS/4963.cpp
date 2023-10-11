ca3c096f99b6965e638359c91b060ec4e038a5b0
yelin56 <100378349+yelin56@users.noreply.github.com>
2023-10-11 19:24:42 +0900

    [dfs&bfs] 10월 11일

--- 07_DFS&BFS/4963.cpp
+++ 07_DFS&BFS/4963.cpp
@@ -3,25 +3,33 @@
 #include <vector>
 
 using namespace std;
-int dx[] = {0,0,-1,1,1,1,-1,-1}; //
-int dy[] = {1,-1,0,0,1,-1,-1,1};
-void dfs(int r, int c, int h, int w, vector<vector<int>> &grape) {
-    grape[r][c] =2;
+int dx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+void dfs(int a, int b, int h, int w, vector<vector<int>> &grape)
+{
+    grape[a][b] = 2; // 방문
 
-    for(int i = 0; i < 8; i++) {
-        int r = r + dx[i];
-        int c = c + dy[i];
+    for (int i = 0; i < 8; i++)
+    {
+        int r = a + dx[i];
+        int c = b + dy[i];
 
-        if(r >= 0 && r < h && c >= 0 && c < w && grape[r][c] == 1) {//found
+        if (r >= 0 && r < h && c >= 0 && c < w && grape[r][c] == 1)
+        { // found
             dfs(r, c, h, w, grape);
         }
     }
 }
-int cnt = 0;
-int cal(int w, int h, vector<vector<int>>&grape){
-    for(int i = 0; i < h; i++) {
-        for(int j = 0; j < w; j++) {
-            if(grape[i][j] == 1) {
+
+int cal(int w, int h, vector<vector<int>> &grape)
+{
+    int cnt = 0;
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (grape[i][j] == 1)
+            { // FOUND
                 cnt++;
                 dfs(i, j, h, w, grape);
             }
@@ -29,18 +37,22 @@ int cal(int w, int h, vector<vector<int>>&grape){
     }
     return cnt;
 }
-int main() {
+int main()
+{
     vector<vector<int>> grape;
-    int w,h;
-    while(1) {
+    int w, h;
+    while (1)
+    {
         cin >> w >> h;
-        if (w == 0 && h == 0) {
+        if (w == 0 && h == 0)
+        {
             break;
         }
-        cnt = 0;
         grape.assign(h, vector<int>(w, 0));
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
                 cin >> grape[i][j];
             }
         }
